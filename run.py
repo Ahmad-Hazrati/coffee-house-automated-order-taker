885f5fbace4470049b9a17f43c2f221a7665970c
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2023-08-12 09:49:34 +0200

    Define function to get the API key from key.ini

--- run.py
+++ run.py
@@ -10,27 +10,27 @@ def _get_api_key():
     return config["openweather"]["api_key"]
 
 
-city = input('Enter city name: ')
+# city = input('Enter city name: ')
 
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+# url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
 
-response = requests.get(url)
+# response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    temp = data['main']['temp']
-    feels = data ['main']['feels_like']
-    desc = data['weather'][0]['description']
-    min_temp = data['main']['temp_min']
-    max_temp = data['main']['temp_max']
-    pres = data['main']['pressure']
-    moisture = data['main']['humidity']
+# if response.status_code == 200:
+#     data = response.json()
+#     temp = data['main']['temp']
+#     feels = data ['main']['feels_like']
+#     desc = data['weather']['description']
+#     min_temp = data['main']['temp_min']
+#     max_temp = data['main']['temp_max']
+#     pres = data['main']['pressure']
+#     moisture = data['main']['humidity']
     
-    print(f'The temperature is {temp}°C and it feels like {feels}°C.')
-    print(f'The weather is {desc} with minimum  temperature of {min_temp}°C and maximum of {max_temp}.°C')
-    print(f'The humidity is {moisture}.')
-else:
-    print('Error fetching weather data')
+#     print(f'The temperature is {temp}°C and it feels like {feels}°C.')
+#     print(f'The weather is {desc} with minimum  temperature of {min_temp}°C and maximum of {max_temp}.°C')
+#     print(f'The humidity is {moisture}.')
+# else:
+#     print('Error fetching weather data')
     
 
 
