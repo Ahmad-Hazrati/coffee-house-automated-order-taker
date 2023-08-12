d3915e2040a72f2b017f1f6cae22dab7be4706f1
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2023-08-12 10:11:22 +0200

    Define function to get user arguments and get the requested data through API

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import requests
 from configparser import ConfigParser
+import argparse
 
 def _get_api_key():
     """ 
@@ -8,6 +9,33 @@ def _get_api_key():
     config = ConfigParser()
     config.read("key.ini")
     return config["openweather"]["api_key"]
+
+def get_user_args():
+    """Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    parser.add_argument(
+        "city", nargs="+", type=str, help="enter the city name"
+    )
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
+
+# ...
+
+if __name__ == "__main__":
+    user_args = get_user_args()
+    print(user_args.city, user_args.imperial)
+
 
 
 # city = input('Enter city name: ')
