23381dfd0d99f12c790dd93a1819e06c95e9a4f5
Hazrati <hazrati@LAPTOP-D1Q9PUT3>
2023-08-21 11:20:24 +0200

    Add conditional statement to check the invalid city name

--- forecast_weather.py
+++ forecast_weather.py
@@ -52,32 +52,39 @@ def display_forecast_weather_info(api_data, location):
     """
     Prints formatted weather information about a city.
     """
-    tprint(location.upper())
-    dates = []
-    for el in api_data["list"]:
-        date = el["dt_txt"].split(" ")[0]
-        if date in dates:
-            continue
-        dates.append(date)
-        sleep(2)
-        temp_city = (el["main"]["temp"] - 273.15)
-        feels_like = (el["main"]["feels_like"] - 273.15)
-        weather_desc = el["weather"][0]["description"]
-        weather_id = el["weather"][0]["id"]
-        humidity = el["main"]['humidity']
-        wind_spd = el["wind"]["speed"]
-        sleep(1)
-        print("-----------------------------------------")
-        print("Weather Status on - {}  ".format(date))
-        print("-----------------------------------------")
-        # Assign the the weather_display function
-        # to the weather_symbol variable
-        weather_symbol = select_weather_display_params(weather_id)
-        print("Temperature will be         : {:.2f} deg C".format(temp_city))
-        print("It will feel like           : {:.2f} deg C".format(feels_like))
-        print("Weather condition will be   :", weather_desc)
-        print("Weather description will be :", weather_symbol)
-        print("Humidity will be            :", humidity, '%')
-        print("And Wind speed will be      :", wind_spd, 'kmph')
-        print("---------------------------------------------------------"
-              "-------\n\n")
+    if api_data["cod"] == "404":
+        print("Invalid City: {}, Please check your city name".format(location))
+    elif api_data["cod"] == "401":
+        print("Invalid API key, Please check your API key")
+    else:
+        tprint(location.upper())
+        dates = []
+        for el in api_data["list"]:
+            date = el["dt_txt"].split(" ")[0]
+            if date in dates:
+                continue
+            dates.append(date)
+            sleep(2)
+            temp_city = (el["main"]["temp"] - 273.15)
+            feels_like = (el["main"]["feels_like"] - 273.15)
+            weather_desc = el["weather"][0]["description"]
+            weather_id = el["weather"][0]["id"]
+            humidity = el["main"]['humidity']
+            wind_spd = el["wind"]["speed"]
+            sleep(1)
+            print("-----------------------------------------")
+            print("Weather Status on - {}  ".format(date))
+            print("-----------------------------------------")
+            # Assign the the weather_display function
+            # to the weather_symbol variable
+            weather_symbol = select_weather_display_params(weather_id)
+            print("Temperature will be         :" +
+                  "{:.2f} deg C".format(temp_city))
+            print("It will feel like           :" +
+                  "{:.2f} deg C".format(feels_like))
+            print("Weather condition will be   :", weather_desc)
+            print("Weather description will be :", weather_symbol)
+            print("Humidity will be            :", humidity, '%')
+            print("And Wind speed will be      :", wind_spd, 'kmph')
+            print("---------------------------------------------------------"
+                  "-------\n\n")
